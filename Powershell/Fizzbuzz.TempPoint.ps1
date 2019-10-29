b2d3db7d1ae6e50e47cb1e3ea858b6182898cfa7
ahowell13 <37151756+ahowell13@users.noreply.github.com>
2019-10-28 20:41:12 -0400

    Fizzbuzz!
    Programming practice

---
+++ Powershell/Fizzbuzz.TempPoint.ps1
@@ -0,0 +1,38 @@
+﻿<#	
+	.NOTES
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2019 v5.6.168
+	 Created on:   	10/28/2019 20:37
+	 Created by:   	austi
+	 Organization: 	
+	 Filename:     	
+	===========================================================================
+	.DESCRIPTION
+		A description of the file.
+#>
+
+
+for ($num = 1; $num -le 100; $num++)
+{
+	
+	if ($num % 3 -eq 0)
+	{
+		
+		Write-Output "Fizz"
+	}
+	
+	
+	if ($num % 5 -eq 0)
+	{
+		
+		Write-Output "Buzz"
+	}
+	
+	if ($num % 5 -and $num % 3 -eq 0)
+	{
+		
+		Write-Output "Fizzbuzz"
+	}
+	
+}
+
