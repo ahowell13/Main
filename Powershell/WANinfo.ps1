58e083bbc056acd02bfc0e01d0bae2e796e30fb0
ahowell13 <37151756+ahowell13@users.noreply.github.com>
2020-01-15 14:09:36 -0500

    WANinfo
    updated

--- Powershell/WANinfo.ps1
+++ Powershell/WANinfo.ps1
@@ -12,7 +12,8 @@
 #>
 
 
-function WANinfo {
+function WANinfo
+{
 	
 	#Get public ip address
 	
@@ -20,15 +21,36 @@ function WANinfo {
 	
 	#Resolve public ip DNS
 	
-	$nameresolve = Resolve-DnsName -Name $ipaddr -Type PTR
+	$nameresolve = (Resolve-DnsName -Name $ipaddr -Type PTR).namehost
 	
 	#resolve wan ip to isp
 	
 	$ISPresult = switch -wildcard ($nameresolve)
 	{
-		"*comcast*" {"Comcast"}
-		"*frontier*" {"Frontier"}
-		"*spectrum*" { "Spectrum" }
-		"*sbcglobal*" {"AT&T"}
+		"*comcast*" { "Comcast"; break }
+		"*frontier*" { "Frontier"; break }
+		"*spectrum*" { "Spectrum"; break }
+		"*sbcglobal*" { "AT&T"; break }
+		default { "Unable to determine ISP"; break }
 	}
+	
+	Write-Host "Your Public IP Address is: " $ipaddr
+	Write-Host "Your ISP is: " $ISPresult
+	
+}
+
+
+function LANinfo
+{
+	
+	#Get Active Interface
+	
+	$activeinterface = Get-CimInstance CIM_NetworkAdapter | Where-Object { $_.PhysicalAdapter -eq "True" -and $_.NetEnabled -eq "True" -and $_.Name -ne "Npcap Loopback Adapter" -and $_.AdapterType -eq "Ethernet 802.3" }
+	
+	#Get info for identified interface
+	
+	$LANinfo = Get-NetIPConfiguration -InterfaceIndex $activeinterface.interfaceindex
+	
+	$LANinfo
+	
 }
