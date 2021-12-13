909203da00286d5c09067810788620d8a07dfdab
Austin Howell <austinrhowelli@gmail.com>
2021-12-13 05:08:34 -0500

    Cleanup

---
+++ Powershell/Small Tools/RepairAllDrives.ps1
@@ -0,0 +1,29 @@
+<#	
+
+	Author: Austin Howell
+	Date: 01.21.2019
+
+	Two functions: One that only scans and reports errors, and one that fixes them.
+
+#>
+
+
+## Find all Physical Drives
+
+
+
+
+function ScanAllDrives {
+
+	(Get-Volume).DriveLetter | ForEach-Object { Repair-Volume -DriveLetter $_ -Scan -ErrorAction SilentlyContinue }
+
+}
+
+
+function FixAllDrives {
+
+(Get-Volume).DriveLetter | ForEach-Object { Repair-Volume -DriveLetter $_ -Spotfix -ErrorAction SilentlyContinue }
+
+}
+
+
---
+++ Powershell/Small Tools/VolumeSearchScript.ps1
@@ -0,0 +1,22 @@
+<# Recursive Directory Search Script
+
+This script will take a search word or phrase and search every drive, including mapped drives, on the computer for the item.
+
+Author: Austin Howell
+Date: 1/20/2019
+
+#>
+
+
+function SearchScript {
+
+	$SearchVariable = Read-Host -prompt "Input your search word or phrase."
+
+	$SearchOutput = (Get-PSDrive -PSProvider Filesystem).root | ForEach-Object { Get-ChildItem -Path $_ -Filter *$SearchVariable* -Recurse -Force -ErrorAction SilentlyContinue }
+
+	$SearchOutput > C:\Users\$env:username\Desktop\Output.txt
+
+}
+
+
+
